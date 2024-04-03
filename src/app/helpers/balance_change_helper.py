53a558e14a6090fb88a3202fb6760d1555efa57e
Kevin aka defron <git.repos@defron.org>
2024-04-02 20:15:54 -0700

    wrong id

--- src/app/helpers/balance_change_helper.py
+++ src/app/helpers/balance_change_helper.py
@@ -9,11 +9,11 @@ def balance_change_helper(
 ) -> Cashflow:
     if account_type.is_asset:
         # TODO: hardcoding transfer ID for now, should revisit
-        if txn_type == TransactionType.Debit and category.id is not 14 and category.parent_category_id is not 14:
+        if txn_type == TransactionType.Debit and category.id is not 22 and category.parent_category_id is not 22:
             cashflow.inflow = amount + (cashflow.inflow or Decimal(0))
         elif txn_type == TransactionType.Debit:
             cashflow.transfer_in = amount + (cashflow.transfer_in or Decimal(0))
-        elif txn_type == TransactionType.Credit and category.id is not 14 and category.parent_category_id is not 14:
+        elif txn_type == TransactionType.Credit and category.id is not 22 and category.parent_category_id is not 22:
             cashflow.outflow = amount + (cashflow.outflow or Decimal(0))
         else:
             cashflow.transfer_out = amount + (cashflow.transfer_out or Decimal(0))
